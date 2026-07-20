bb3b75ad7422f0e2e74b10d71378598b6ea475c3
Morgan Roderick <morgan@roderick.dk>
2026-07-20 19:40:47 +0200

    chore: remove Jekyll steps from version hook

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,13 +7,4 @@ changes --commits --footer
 # Tidy CHANGES.md after any manual hand-editing
 prettier --write CHANGES.md
 
-echo 'set new current/next release id in documentation'
-node ./scripts/set-release-id-in-config-yml.cjs
-
-echo 'update changelog'
-./scripts/update-changelog-page.sh
-
-# Add our updates
-git add docs/changelog.md
-git add docs/_config.yml
 git add CHANGES.md
